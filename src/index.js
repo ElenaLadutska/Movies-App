9dccc360441549336040203816ed037b83baf36f
ElenaLadutska <meduskalena@gmail.com>
2020-09-13 19:49:34 +0300

    Something changed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from 'react-router-dom';
 import './style.css';
 import {MoviesApp} from './components/MoviesApp'
 
 ReactDOM.render(
-    <BrowserRouter>
+    <React.StrictMode>
         <MoviesApp/>
-    </BrowserRouter>,
+    </React.StrictMode>,
     document.getElementById(
         'root'
     )
